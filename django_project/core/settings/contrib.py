7a81c32840280cb29e10e452ab5a402134433ff0
Mauricio Vidulin <mauricio.vidulin@gmail.com>
2015-03-28 22:38:54 +0100

    Initial Django commit

---
+++ django_project/core/settings/contrib.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from .base import *  # NOQA
+
+# Extra installed apps
+INSTALLED_APPS += (
+    # any 3rd party apps
+)
+
+# 3rd party specific app settings
---
+++ django_project/core/settings/dev.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from .project import *  # NOQA
+
+# Set debug to True for development
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+LOGGING_OUTPUT_ENABLED = DEBUG
+LOGGING_LOG_SQL = DEBUG
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Disable caching while in development
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        # define output formats
+        'verbose': {
+            'format': (
+                '%(levelname)s %(name)s %(asctime)s %(module)s %(process)d '
+                '%(thread)d %(message)s')
+        },
+        'simple': {
+            'format': (
+                '%(name)s %(levelname)s %(filename)s L%(lineno)s: '
+                '%(message)s')
+        },
+    },
+    'handlers': {
+        # console output
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'level': 'DEBUG',
+        },
+        # 'logfile': {
+        #     'class': 'logging.FileHandler',
+        #     'filename': '/tmp/app-dev.log',
+        #     'formatter': 'simple',
+        #     'level': 'DEBUG',
+        # }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO',  # switch to DEBUG to show actual SQL
+        },
+        # example app logger
+        # 'app.module': {
+        #     'level': 'INFO',
+        #     'handlers': ['logfile'],
+        #     # propagate is True by default, which proppagates logs upstream
+        #     'propagate': False
+        # }
+    },
+    # root logger
+    # non handled logs will propagate to the root logger
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING'
+    }
+}
